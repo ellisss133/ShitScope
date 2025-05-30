b605116ee380c192acbfb648882c70a74d0f82ce
Vadim <vadimogienko42k@gmail.com>
2025-05-30 20:06:57 +0700

    Начало работы с UI

---
+++ MainForm.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Shitscope
+{
+  public partial class MainForm : Form
+  {
+    private Button changeZodiacButton;
+    private Label infoLabel;
+    private PictureBox catPicture;
+
+    private Label loadingLabel;
+    private Timer loadingDotsTimer;
+    private int dotCount = 0;
+
+    private Label catCaption;
+    private Panel catPanel;
+
+    public MainForm()
+    {
+      InitializeComponent();
+      SetupCustomUI();
+
+      this.Resize += (s, e) =>
+      {
+        CenterLoadingLabel();
+      };
+    }
+
+    
+  }
+}
